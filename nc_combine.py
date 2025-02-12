8470b45ecb316f0ba1be6be0400e0fabad7739bc
Wuwenchie <161718523+Wuwenchie@users.noreply.github.com>
2025-02-12 10:46:07 +0800

    Update nc_combine.py

--- nc_combine.py
+++ nc_combine.py
@@ -12,13 +12,13 @@ for file_name in os.listdir(file_paths):
     file_path.append(r'D:/NOAA_file2023/01/'+file_name)
 file_path ##输出文件路徑
 
-cmorph_new = [] ##建立一个空列表，存储逐日降雨数据
+data_new = [] ##建立一个空列表，存储逐日降雨数据
 for i in range(len(file_path)):
-    cmorph = xr.open_dataset(file_path[i])['cmorph']
-    cmorph_new.append(cmorph) ##存储每日的降雨数据
-da = xr.concat(cmorph_new,dim='time') ##将数据以时间维度来进行拼接
+    data = xr.open_dataset(file_path[i])['sos']
+    data_new.append(data) ##存储每日的降雨数据
+da = xr.concat(data_new,dim='time') ##将數據以時間维度来進行拼接
 #print(da)
 path_new = r"./data/training_data" ##设置新路径
 #print(da.variable)
-da.to_netcdf(path_new + 'CMORPH_6.nc') ##对拼接好的文件进行存储
+da.to_netcdf(path_new + 'sos_1850_2009.nc') ##对拼接好的文件进行存储
 da.close() ##关闭文件
